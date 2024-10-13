2f447112713c233cef624f4ee1b38391f79d3741
perkzen <perko.domen@gmail.com>
2024-10-13 21:50:19 +0200

    fix: lat,lng validator

--- src/lib/validators/add-location.ts
+++ src/lib/validators/add-location.ts
@@ -9,18 +9,13 @@ export const AddLocationValidator = z
       .trim()
       .min(2, { message: 'Address must be at least 2 characters long' })
       .max(255, { message: 'Address must be less than 255 characters long' }),
-    lat: z
-      .number()
-      .refine((val) => val >= -90 && val <= 90, {
-        message: 'Latitude must be between -90 and 90',
-      })
-      .optional(),
-    lng: z
-      .number()
-      .refine((val) => val >= -180 && val <= 180, {
-        message: 'Longitude must be between -180 and 180',
-      })
-      .optional(),
+    lat: z.number().refine((val) => val >= -90 && val <= 90, {
+      message: 'Latitude must be between -90 and 90',
+    }),
+    lng: z.number().refine((val) => val >= -180 && val <= 180, {
+      message: 'Longitude must be between -180 and 180',
+    }),
+
     fileList: ImageValidator(MAX_FILE_SIZE_BYTES, ['image/jpeg', 'image/png']),
   })
   .refine(
