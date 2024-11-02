70609613705caec3dba2102bf192c97c7cd62fe0
perkzen <perko.domen@gmail.com>
2024-11-02 13:12:19 +0100

    fix: navbar

--- src/components/blocks/navbar/navbar.tsx
+++ src/components/blocks/navbar/navbar.tsx
@@ -1,9 +1,13 @@
+'use client';
 import { FC, ReactNode } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useMediaQuery, useTheme } from '@mui/material';
 import classNames from 'classnames';
-import Logo from 'public/images/logo.svg';
+import DesktopLogo from 'public/images/logo.svg';
+import MobileLogo from 'public/images/mobile-logo.svg';
 import { Routes } from '@/lib/constants/routes';
+import { useSessionStore } from '@/lib/stores/session-store';
 import styles from './navbar.module.scss';
 
 type NavbarProps = {
@@ -12,15 +16,19 @@ type NavbarProps = {
 };
 
 const Navbar: FC<NavbarProps> = ({ className, items }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const session = useSessionStore((state) => state.session);
+  const isAuth = session !== null;
+
   return (
     <nav className={classNames(styles.container, className)}>
       <Link href={Routes.HOME}>
         <Image
-          src={Logo}
+          src={isAuth && isMobile ? MobileLogo : DesktopLogo}
           alt={'logo'}
-          height={40}
           quality={100}
-          className={styles.logo}
         />
       </Link>
       {items}
